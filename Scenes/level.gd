59bc2c0176c6e0ab67faaaf451a5dddbdfae9a15
Caina Caldas <cainacc@hotmail.com>
2023-09-30 23:10:28 -0300

    many things

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -1,5 +1,6 @@
-extends Area2D
+extends Node2D
 
+@onready var ship_start = $ship_start_point
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,6 +10,3 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func _on_body_entered(body):
-	print("player entered portal")
