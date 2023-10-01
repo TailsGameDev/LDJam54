f07c3dfa2ce65344189456c4f11475d2b7c987ec
Caina Caldas <cainacc@hotmail.com>
2023-09-30 22:43:35 -0300

    something certainly, but not pretty much sure what

--- Scripts/block.gd
+++ Scripts/block.gd
@@ -1,17 +1,22 @@
 extends Area2D
 
+@export var velocity = Vector2(0,0)
 @export var force_multipliers = Vector2(0,1)
 
 var ship_vel
 var ship
 
 func _physics_process(delta):
+	# push ship away
 	if self.ship != null:
 		if ship.get_vel_length() < ship_vel.length():
 			var force_x = ship_vel.x * force_multipliers.x
 			var force_y = ship_vel.y * force_multipliers.y
 			var force = Vector2(force_x, force_y)
 			ship.apply_external_force(force)
+	
+	# move block
+	self.position = self.position + self.velocity
 
 func _on_body_entered(body):
 	#print("enter")
