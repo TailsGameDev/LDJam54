0bae87b185711dbaeaa4a6b22b9794714d92a3dc
Caina Caldas <cainacc@hotmail.com>
2023-10-01 12:40:06 -0300

    fase 2

--- Scripts/game_god.gd
+++ Scripts/game_god.gd
@@ -23,13 +23,18 @@ func load_next_level():
 	var level_scene = self.level_scenes[self.level_index]
 	self.level_index = (self.level_index + 1) % len(self.level_scenes)
 	var level = level_scene.instantiate()
+	level.ending_portal_reached.connect(on_ending_portal_reached)
 	add_child(level)
+	#call_deferred("add_child", level)
+	
 	if self.ship != null:
 		self.ship.destroy_self()
 	self.ship = ship_scene.instantiate()
 	add_child(self.ship)
+	#call_deferred("add_child", self.ship)
+
 	self.ship.external_set_position(level.ship_start.position)
-	level.ending_portal_reached.connect(on_ending_portal_reached)
+	
 
 func on_ending_portal_reached():
 	load_next_level()
